dc1b7d6dbe4e28a18228c6b775cfce058c5ee8a9
AntonBoreiko <1213.96@mail.ru>
2021-08-24 18:52:27 +0300

    Add Tabs menu

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,4 +1,37 @@
 import './script'
 
-console.log('app')
-console.log('dfgf')
+window.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.body.querySelectorAll('.tabheader__item')
+  const tabsContent = document.querySelectorAll('.tabcontent')
+  const tabsParent = document.querySelector('.tabheader__items')
+
+  function hideTabConstent() {
+    tabsContent.forEach(item => {
+      item.style.display = 'none'
+    })
+
+    tabs.forEach(item => {
+      item.classList.remove('tabheader__item_active')
+    })
+  }
+
+  function showTabConstent(i = 0) {
+    tabsContent[i].style.display = 'block'
+    tabs[i].classList.add('tabheader__item_active')
+  }
+  hideTabConstent()
+  showTabConstent()
+
+  tabsParent.addEventListener('click', (event) => {
+    const target = event.target
+
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabConstent()
+          showTabConstent(i)
+        }
+      })
+    }
+  })
+})
